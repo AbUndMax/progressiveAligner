7566a82da81b03f39d95e48ee4c029fb3a905392
Niklas <Max_Niklas@outlook.de>
2024-12-12 11:08:50 +0100

    Implement lazy-loading for each enum-BLOSUM hashMap

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,11 +1,14 @@
 import org.junit.jupiter.api.Test;
+import progressiveAligner.ToolClasses.ScoreMatrix;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestClass {
 
     @Test
     public void test() {
-        assertTrue(true);
+
+        double percent = ScoreMatrix.computePercentIdentity("ATCGGACGTA", "ATCGGGCGTA");
+        assertEquals(0.9, percent, 0.000001);
     }
 }
